7b93fcbf57de80f1ff276ead36e49b7964374d5c
Muhammad Wahyudin <muhammad.whydn@gmail.com>
2019-10-25 09:12:18 +0700

    Update gradle & readme for kotlin extension

--- README.md
+++ README.md
@@ -2,30 +2,31 @@
 [![](https://jitpack.io/v/hyuwah/DraggableView.svg)](https://jitpack.io/#hyuwah/DraggableView)
 
 DraggableView is an Android library to make floating draggable view easy, currently it only extends from ImageView.
+If you're using kotlin there's also an extension to make any view draggable.
 
 ## Setup
 
 On root / project **build.gradle** file add `maven { url 'https://jitpack.io' }`
 
 Example:
-```
-	allprojects {
-		repositories {
-			...
-			maven { url 'https://jitpack.io' }
-		}
-	}
+```gradle
+allprojects {
+    repositories {
+        ...
+        maven { url 'https://jitpack.io' }
+    }
+}
 ```
 
 On app **build.gradle** add `implementation 'com.github.hyuwah:DraggableView:LatestVersion'`
 
 Example:
-```
+```gradle
 dependencies {
-            ...
-	        implementation 'com.github.hyuwah:DraggableView:LatestVersion'
-	        ...
-	}
+    ...
+    implementation 'com.github.hyuwah:DraggableView:LatestVersion'
+    ...
+}
 ```
 
 **Note:** check the number on jitpack badge above for latest version
@@ -43,25 +44,61 @@ Sticky Axis | NON_STICKY, STICKY_AXIS_X, STICKY_AXIS_Y, STICKY_AXIS_XY (NON_STIC
 
 
 ### Basic sample
+
+#### Using xml view (DraggableImageView)
+
 On Layout XML file
-```
+```xml
 <io.github.hyuwah.draggableviewlib.DraggableImageView
             android:src="@mipmap/ic_launcher_round"
             android:id="@+id/draggableView"
             android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-    />
+            android:layout_height="wrap_content"/>
 ```
 
 On Activity / Fragment file
-```
+```kotlin
 var dv = findViewById<DraggableImageView>(R.id.draggableView)
 dv.setOnClickListener {
     // TODO on click
 }
 ```
 
-Check `example` module for actual implementation
+#### Using extension (Kotlin only)
+
+You can extent any view or viewgroup to be draggable (i.e. Button, FrameLayout, Linearlayout, LottieView, etc)
+
+Here's some example using TextView:
+
+```xml
+<TextView
+            android:id="@+id/tv_test_draggable"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:text="DRAG ME!" />
+```
+
+```kotlin
+var tv = findViewById<TextView>(R.id.tv_test_draggable)
+
+tv.makeDraggable(Constants.STICKY.AXIS_X, true)
+
+// First param is the axis:
+// - Constants.STICKY.AXIS_X
+// - Constants.STICKY.AXIS_Y
+// - Constants.STICKY.AXIS_XY
+// - Constants.STICKY.NONE
+
+// Second param is animation toggle
+// - true or false
+```
+
+
+Check [example](https://github.com/hyuwah/DraggableView/blob/master/example/src/main/java/io/github/hyuwah/draggableview/MainActivity.kt) module for actual implementation
+
+## Accompanying Article
+
+* [Implementasi DraggableView di Android (Bahasa)](https://medium.com/@hyuwah/implementasi-draggable-view-di-android-eb84e50fbba9)
 
 ## License
 `DraggableView` is available under the MIT license. See the [LICENSE](https://github.com/hyuwah/DraggableView/blob/master/LICENSE) file for more info.
--- example/src/main/java/io/github/hyuwah/draggableview/MainActivity.kt
+++ example/src/main/java/io/github/hyuwah/draggableview/MainActivity.kt
@@ -12,54 +12,89 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var currentStickyAxis = Constants.STICKY.AXIS_X
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var dvTest = findViewById<DraggableImageView>(R.id.dv_test)
+        // Make any view Draggable via Kotlin Extension
+        ll_test_draggable.makeDraggable(currentStickyAxis, true)
+        tv_test_draggable.makeDraggable(currentStickyAxis, true)
 
-        var swAnimate = findViewById<Switch>(R.id.sw_animate)
-        var btnStickyX= findViewById<Button>(R.id.btn_sticky_x)
-        var btnStickyY= findViewById<Button>(R.id.btn_sticky_y)
-        var btnStickyXY= findViewById<Button>(R.id.btn_sticky_xy)
-        var btnNonSticky= findViewById<Button>(R.id.btn_nonsticky)
+        // Via DraggableImageView in activity_main.xml
+        val dvTest = findViewById<DraggableImageView>(R.id.dv_test)
+
+        val swAnimate = findViewById<Switch>(R.id.sw_animate)
+        val btnStickyX = findViewById<Button>(R.id.btn_sticky_x)
+        val btnStickyY = findViewById<Button>(R.id.btn_sticky_y)
+        val btnStickyXY = findViewById<Button>(R.id.btn_sticky_xy)
+        val btnNonSticky = findViewById<Button>(R.id.btn_nonsticky)
 
         swAnimate.isChecked = dvTest.isAnimate()
+
         swAnimate.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 dvTest.setAnimate(true)
-                tv_test_draggable.makeDraggable(Constants.STICKY.AXIS_X, true)
+                ll_test_draggable.makeDraggable(currentStickyAxis, true)
+                tv_test_draggable.makeDraggable(currentStickyAxis, true)
             } else {
-                tv_test_draggable.makeDraggable(Constants.STICKY.AXIS_X, false)
                 dvTest.setAnimate(false)
+                ll_test_draggable.makeDraggable(currentStickyAxis, false)
+                tv_test_draggable.makeDraggable(currentStickyAxis, false)
             }
         }
 
+        // Set sticky axis to X axis
         btnStickyX.setOnClickListener {
             dvTest.setStickyAxis(DraggableImageView.STICKY_AXIS_X)
+            currentStickyAxis = Constants.STICKY.AXIS_X
+            ll_test_draggable.makeDraggable(currentStickyAxis, swAnimate.isChecked)
+            tv_test_draggable.makeDraggable(currentStickyAxis, swAnimate.isChecked)
             Toast.makeText(this@MainActivity, "Sticky Axis set to X", Toast.LENGTH_SHORT).show()
         }
 
+        // Set sticky axis to Y axis
         btnStickyY.setOnClickListener {
             dvTest.setStickyAxis(DraggableImageView.STICKY_AXIS_Y)
+            currentStickyAxis = Constants.STICKY.AXIS_Y
+            ll_test_draggable.makeDraggable(currentStickyAxis, swAnimate.isChecked)
+            tv_test_draggable.makeDraggable(currentStickyAxis, swAnimate.isChecked)
             Toast.makeText(this@MainActivity, "Sticky Axis set to Y", Toast.LENGTH_SHORT).show()
         }
 
+        // Set sticky axis to XY axis
         btnStickyXY.setOnClickListener {
             dvTest.setStickyAxis(DraggableImageView.STICKY_AXIS_XY)
+            currentStickyAxis = Constants.STICKY.AXIS_XY
+            ll_test_draggable.makeDraggable(currentStickyAxis, swAnimate.isChecked)
+            tv_test_draggable.makeDraggable(currentStickyAxis, swAnimate.isChecked)
             Toast.makeText(this@MainActivity, "Sticky Axis set to XY", Toast.LENGTH_SHORT).show()
         }
 
+        // Remove sticky axis / will float
         btnNonSticky.setOnClickListener {
             dvTest.setStickyAxis(DraggableImageView.NON_STICKY)
+            currentStickyAxis = Constants.STICKY.NONE
+            ll_test_draggable.makeDraggable(currentStickyAxis, swAnimate.isChecked)
+            tv_test_draggable.makeDraggable(currentStickyAxis, swAnimate.isChecked)
             Toast.makeText(this@MainActivity, "Sticky Axis set none", Toast.LENGTH_SHORT).show()
         }
 
+        // Set click listener
         dvTest.setOnClickListener {
             Toast.makeText(this@MainActivity, "Clicked", Toast.LENGTH_SHORT).show()
         }
 
-        ll_test_draggable.makeDraggable(Constants.STICKY.AXIS_Y)
-        tv_test_draggable.makeDraggable(Constants.STICKY.AXIS_X, true)
+        ll_test_draggable.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Draggable LinearLayout Clicked", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        tv_test_draggable.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Draggable Textview Clicked", Toast.LENGTH_SHORT)
+                .show()
+        }
+
     }
 }
